04f15e2749e7dbb563b03c6579f5f273d389f72d
Adrian <adrian.mojica@gmail.com>
2014-02-27 17:19:29 -0500

    add address

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,9 +1,10 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description, :image
+  attr_accessible :description, :image, :address
 
 
   validates :description, presence: true
   validates :user_id, presence: true
+  validates :address, presence: true
   validates_attachment :image, presence: true,
   							   content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']},
   							   size: { less_than: 5.megabytes }
